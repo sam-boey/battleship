b1a009c8b7417d11ff98763ed70a1a5da33bc34e
samboey <samuelkjboey@gmail.com>
2019-07-09 17:30:59 +0800

    sound added and random pattern

--- script.js
+++ script.js
@@ -17,6 +17,60 @@ var board = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
+var board1 = [
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+var board2 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 1, 1, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1, 1, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+];
+
+var board3 = [
+    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+];
+
+var board4 = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+];
+
+var boardArr = [board, board1, board2, board3, board4]
+
 const boxes = document.querySelectorAll('.box');
 startGame();
 
@@ -25,9 +79,14 @@ function countRounds() {
     for (var i = 0; i < boxes.length; i++) {
         boxes[i].addEventListener("click", function() {
             count++;
-            displayCount.innerHTML = "Number of torpedoes used " + count + "/30";
+            displayCount.innerHTML = "Number of torpedoes used : " + count + "/30";
+            if (count === 30) {
+                alert("You ran out of torpedoes! You lost!");
+                gameRestart();
+            }
         })
     }
+
 }
 countRounds();
 
@@ -39,39 +98,42 @@ function startGame() {
         boxes[i].addEventListener('click', fillClick, false); //click function
     };
     count = 0;
-    displayCount.innerHTML = "Number of torpedoes used " + count + "/30";
-    targets= 20;
-    displayTarget.innerHTML = "Targets remaining " + targets + "/20";
+    displayCount.innerHTML = "Number of torpedoes used : " + count + "/30";
+    targets = 20;
+    displayTarget.innerHTML = "Targets remaining : " + targets + "/20";
 }
 
 //fire target
+var randomNum = Math.floor(Math.random() * 5);
+var playingBoard = boardArr[randomNum];
+console.log(playingBoard)
+
+//sound files
+var fart = new Audio('sounds/fart.mp3');
+var explosion = new Audio('sounds/explosion.mp3');
+
 function fillClick(event) {
-    var position = board[parseInt(event.target.getAttribute("data-row"))][parseInt(event.target.getAttribute("data-col"))];
+    var position = playingBoard[parseInt(event.target.getAttribute("data-row"))][parseInt(event.target.getAttribute("data-col"))];
     console.log(event.target)
     if (event.target.style.backgroundColor === "rgba(128, 128, 128, 0.7)" || event.target.style.backgroundColor === "rgba(128, 0, 0, 0.7)") {
         alert("Stop wasting your torpedoes! You already fired at this location.");
     } else if (position === 0) { //when user clicks on empty cell (missed)
+        fart.play();
         event.target.style.backgroundColor = "rgba(128,128,128,0.7)";
-        displayTarget.innerHTML = "Targets remaining " + targets + "/20";
+        displayTarget.innerHTML = "Targets remaining : " + targets + "/20";
     } else if (position === 1) { //when user click on occupied cell (hit)
+        explosion.play();
         event.target.style.backgroundColor = "rgba(128,0,0,0.7)";
-        targets --;
-        displayTarget.innerHTML = "Targets remaining " + targets + "/20";
+        targets--;
+        displayTarget.innerHTML = "Targets remaining : " + targets + "/20";
         alert("You have hit a ship!");
+        if (targets === 0) {
+            alert("All the enemy ships have been sunk! You are victorious!");
+            gameRestart();
+        }
     }
     turn++; //adds a turn every round
 }
-//targets remaining
-// function countTargets() {
-//     var position = board[parseInt(event.target.getAttribute("data-row"))][parseInt(event.target.getAttribute("data-col"))];
-//     for (position === 1) {
-//         boxes[i].addEventListener("click", function() {
-//             targets--;
-//             displayTarget.innerHTML = "Targets remaining " + targets + "/20";
-//         })
-//     }
-// }
-
 
 var restartButton = document.getElementById("reset");
 restartButton.addEventListener("click", gameRestart);
